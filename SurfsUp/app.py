e964c84fb0b1d2041773eb1a5945c3c0ac9b6b87
W. Matthew Mills <30300016+WMatthewARC@users.noreply.github.com>
2022-08-24 16:30:33 -0400

    Update app.py

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,2 +1,21 @@
-#Matt's main bit? 
+## Matt's app.py
+ 
+from flask import Flask, jsonify
+
+app = Flask (__name__)
+
+# home route
+@app.route("/")
+def home():
+    return("Welcome to the Hawaii Climate Analysis Local API!")
+    
+
+
+
+
+
+## app launcher
+
+if __name__ == '__main__':
+        app.run()
 

efe161df7bab13fc25ce3a35aa1d34a168e20162
W. Matthew Mills <30300016+WMatthewARC@users.noreply.github.com>
2022-08-26 12:31:22 -0400

    Update app.py

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -12,7 +12,6 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, inspect, func, select
 
-
 app = Flask (__name__)
 
 # conncted to database
